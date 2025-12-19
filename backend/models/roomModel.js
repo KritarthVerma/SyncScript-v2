4e9437455ea59d0958ca87e3fb48ef7dd667261a
KritarthVerma <kritarth4444@gmail.com>
2025-12-19 16:06:05 +0530

    Created Room Model

---
+++ backend/models/roomModel.js
@@ -0,0 +1,36 @@
+// models/Room.js
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+  {
+    // Public sharable room id (like code)
+    externalId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true, 
+    },
+
+    // Users associated with this room
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    // Shared language + code state
+    roomSettingsId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Settings",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Room", roomSchema);
