00541db494d7d1782d161f6f4341d608d9e9aedd
KritarthVerma <kritarth4444@gmail.com>
2025-12-19 12:27:37 +0530

    Server Setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors()); //Enables Cross-Origin Resource Sharing so your backend can accept requests from different domains
+app.use(express.json()); //Parses incoming JSON request bodies and makes the data available in req.body.
+
+//Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Server
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
